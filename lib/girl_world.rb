691560127e085939a7f5e712df3ba4931344d649
Amal Hussein <amalhussein08@gmail.com>
2013-11-25 18:24:37 -0500

    cleaned up methods some more

--- lib/girl_world.rb
+++ lib/girl_world.rb
@@ -11,30 +11,37 @@ module GirlWorld
 # Usage:
 # my_high_school = GirlWorld::HighSchool.new()
 # my_high_school
-	class HighSchool
-		attr_reader :url 
+	class SecondarySchool
+
+		attr_reader :url :indicator
 		
-		def initialize()
+		def initialize(url, indicator)
 			@url = 'http://api.worldbank.org/countries'
-			@indicator = 'indicators/SE.ENR.PRSC.FM.ZS'
+			@indicator = 'indicators/SE.ENR.PRSC.FM.ZS' #indicator for ratio of girls/boys in secondary school 
 		end 
 		
 		#gets girl/boy ratio by country from user input
 		def ratio(country)
-			HTTParty.get("#{url}/#{country}#{indicator}?&format=json")
+			HTTParty.get("#{url}/#{country}/#{indicator}?&format=json")
 		end 
 
 		#gets girl/boy ratio by country, and year range from user input
 		def ratio_year_range(country, start_year, end_year)
-			HTTParty.get("#{url}/#{country}/indicators/SE.ENR.PRSC.FM.ZS?date=#{start_year}:#{end_year}&format=json")																																													
+			HTTParty.get("#{url}/#{country}/#{indicator}?date=#{start_year}:#{end_year}&format=json")																																													
 		end  
 
-		#gets all info for all countries and regions
-		def ratio_all()
+		#gets all info for all countries and regions from user input
+		def ratio_all
+			HTTParty.get("#{url}/all/#{indicator}?&format=json")
+		end 
+
+		#gets all info for all countries and regions by year range from user input
+		def ratio_all_year_range
+			HTTParty.get("#{url}/all/#{indicator}?date=#{start_year}:#{end_year}&format=json")
 		end 
 
 	end
-	#gets girl/bou high school rate
+
 	
 	def poplution(country)
 	end 
