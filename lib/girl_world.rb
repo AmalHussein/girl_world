b659b7f118b6ee1fed687a2b3bee70dcf9c46594
Amal Hussein <amalhussein08@gmail.com>
2013-11-25 18:18:35 -0500

    working methods, d.r.y-ing up code, and adding class, and attribute accessor methods

--- lib/girl_world.rb
+++ lib/girl_world.rb
@@ -3,42 +3,39 @@ require 'httparty'
 #require 'girl_world'
 
 module GirlWorld
-	#Format for the defination of an indicator indicators 
-	# http://api.worldbank.org/indicators/SP.POP.TOTL
-	#ex => http://api.worldbank.org/indicators/SE.ENR.PRSC.FM.ZS for girl/boy ratio for high school 
-
-	#Return a list of all indicators
-	# http://api.worldbank.org/indicators
-
-	# Return information about GDP in current US dollars
-	# http://api.worldbank.org/indicators/NY.GDP.MKTP.CD
-
-	# Return the definition of the indicator SP.POP.TOTL, in this case, total population
-	# http://api.worldbank.org/indicators/SP.POP.TOTL
-
-	# Display all indicators for the source: Doing Business referred by numeric ID
-	# http://api.worldbank.org/source/1/indicators
-
-	# Return the GDP (in current US$) for Brazil (this call also accepts the 3 letter ISO code)
-	# http://api.worldbank.org/countries/br/indicators/NY.GDP.MKTP.CD
-
-	# Return the GDP for ALL countries (please note the "all" special keyword) in 2002
-	# http://api.worldbank.org/countries/all/indicators/NY.GDP.MKTP.CD?date=2002:2002
-
-	# In order to get one indicator for a country - for example GNI per capita for Brazil in current US$ using the Atlas method
-	# http://api.worldbank.org/countries/br/indicators/NY.GNP.PCAP.CD/?date=2006:2006
-
-	# By default, the GNI call above gives all the data available. If you want just the GNI data from, say, 1960 to 1970
-	# http://api.worldbank.org/countries/br/indicators/NY.GNP.PCAP.CD?date=1960:1970
-
-	def self.high_school_ratio(country)
-		HTTParty.get("http://api.worldbank.org/countries/#{country}/indicators/SE.ENR.PRSC.FM.ZS?&format=json")
+	
+	def self.percent_in_parliement(country)
+		HTTParty.get("http://api.worldbank.org/countries/#{country}/indicators/SG.GEN.PARL.ZS?&format=json")
 	end 
 
-	def self.high_school_ratio_date(country, start_year, end_year)
-		HTTParty.get("http://api.worldbank.org/countries/#{country}/indicators/SE.ENR.PRSC.FM.ZS?date=#{start_year}:#{end_year}&format=json")																																													
-	end 
+# Usage:
+# my_high_school = GirlWorld::HighSchool.new()
+# my_high_school
+	class HighSchool
+		attr_reader :url 
+		
+		def initialize()
+			@url = 'http://api.worldbank.org/countries'
+			@indicator = 'indicators/SE.ENR.PRSC.FM.ZS'
+		end 
+		
+		#gets girl/boy ratio by country from user input
+		def ratio(country)
+			HTTParty.get("#{url}/#{country}#{indicator}?&format=json")
+		end 
 
+		#gets girl/boy ratio by country, and year range from user input
+		def ratio_year_range(country, start_year, end_year)
+			HTTParty.get("#{url}/#{country}/indicators/SE.ENR.PRSC.FM.ZS?date=#{start_year}:#{end_year}&format=json")																																													
+		end  
+
+		#gets all info for all countries and regions
+		def ratio_all()
+		end 
+
+	end
+	#gets girl/bou high school rate
+	
 	def poplution(country)
 	end 
 
@@ -46,7 +43,7 @@ end
 
 puts GirlWorld.high_school_ratio('us')
 
-
+puts GirlWorld.high_school_ratio_by_date('us', 2000, 2013)
 
 
 
